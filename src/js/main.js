24e825e7a51ee7c0e818b248efe31e1cc695026d
Andrei Viktorov <aivictorov@yandex.ru>
2023-04-24 18:14:04 +0400

    middle-of-day 24.04

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,36 @@
 window.addEventListener('DOMContentLoaded', () => {
 	modalWindows();
 	alignModalWindows();
-	// tabs();
-	// clientsCarousel();
-	// partnersCarousel();
+	dropdowns()
+	dualRangeInputs();
+	tabs();
+	// carousel();
 });
+
+function dropdowns() {
+	document.querySelectorAll('button[dropdown-button]').forEach((button) => {
+		const dropdown = document.querySelector(`div[dropdown="${button.getAttribute('dropdown-button')}"]`);
+
+		if (dropdown) {
+			button.addEventListener('click', (event) => {
+				event.stopPropagation();
+				dropdown.classList.toggle('active');
+			});
+
+			dropdown.addEventListener('click', (event) => {
+				event.stopPropagation();
+			});
+
+			document.addEventListener('click', () => {
+				dropdown.classList.remove('active');
+			});
+
+			document.addEventListener('keydown', (event) => {
+				if (event.key === 'Escape')	dropdown.classList.remove('active');
+			});
+		};
+	});
+};
 
 function modalWindows() {
 	document.querySelectorAll('button[modal-button]').forEach((button) => {
@@ -46,20 +72,70 @@ function alignModalWindows() {
 	};
 };
 
-// function tabs() {
-// 	document.querySelectorAll('.tab').forEach((tab) => {
-// 		const button = tab.querySelector('.tab__button');
-// 		const icon = tab.querySelector('.tab__icon');
-// 		const content = tab.querySelector('.tab__content');
+function dualRangeInputs() {
+	const dualRangeInputs = document.querySelectorAll('.dual-range-input');
+	
+	dualRangeInputs.forEach((input) => {
+		const rangeOne = input.querySelector('[data-name="range-1"]');
+		const rangeTwo = input.querySelector('[data-name="range-2"]');
+		const valueOne = input.querySelector('[data-name="value-1"]');
+		const valueTwo = input.querySelector('[data-name="value-2"]');
+		const minGap = 0;
+		const track = input.querySelector('[data-name="track"]');
+		const maxValue = rangeOne.max;
+	
+		slideOne();
+		slideTwo();
+	
+		rangeOne.addEventListener('input', slideOne);
+		rangeTwo.addEventListener('input', slideTwo);
+	
+		function slideOne() {
+			if (parseInt(rangeTwo.value) - parseInt(rangeOne.value) <= minGap) {
+				rangeOne.value = parseInt(rangeTwo.value) - minGap;
+			}
+			valueOne.textContent = formatValue(rangeOne.value);
+			fillColor();
+		};
+	
+		function slideTwo() {
+			if (parseInt(rangeTwo.value) - parseInt(rangeOne.value) <= minGap) {
+				rangeTwo.value = parseInt(rangeOne.value) - minGap;
+			}
+			valueTwo.textContent = formatValue(rangeTwo.value);
+			fillColor();
+		};
+	
+		function fillColor() {
+			const percent1 = rangeOne.value / maxValue * 100;
+			const percent2 = rangeTwo.value / maxValue * 100;
+			track.style.background = `linear-gradient(to right, #E1E1E1 ${percent1}%,  #112211 ${percent1}%,
+				#112211 ${percent2}%,  #E1E1E1 ${percent2}%)`;
+		};
+	
+		function formatValue(value) {
+			return value + '$';
+		};
+	})
+};
 
-// 		button.addEventListener('click', () => {
-// 			icon.classList.toggle('active');
-// 			content.classList.toggle('active');
-// 		});
-// 	});
-// };
+function tabs() {
+	document.querySelectorAll('[tabs-container]').forEach((tabsContainer) => {
+		const tabButtons = tabsContainer.querySelectorAll('[tab-button]')
+		tabButtons.forEach((tabButton) => {
+			tabButton.addEventListener('click', () => {
+				tabButtons.forEach((item)=>{item.classList.remove('active')});
+				tabButton.classList.add('active');
+				tabsContainer.querySelectorAll('[tab-content]').forEach((tabContent) => {
+					tabContent.classList.add('none');
+					tabsContainer.querySelector(`[tab-content="${tabButton.getAttribute('tab-button')}"]`).classList.remove('none');
+				});
+			});
+		});
+	});
+};
 
-// function clientsCarousel() {
+// function carousel() {
 // 	const owl = $(".clients .owl-carousel");
 // 	const btnPrev = $(".clients .slider__btn-prev");
 // 	const btnNext = $(".clients .slider__btn-next");
